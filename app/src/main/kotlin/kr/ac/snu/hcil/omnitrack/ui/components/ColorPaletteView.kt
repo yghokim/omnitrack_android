728c051a1d98bb8b00d8e69e25bb1f1f5896ed8c
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-07-19 15:47:26 +0900

    ColorPaletteView implemented

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/ColorPaletteView.kt
@@ -0,0 +1,112 @@
+package kr.ac.snu.hcil.omnitrack.ui.components
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kr.ac.snu.hcil.omnitrack.R
+import kr.ac.snu.hcil.omnitrack.utils.events.Event
+import java.util.*
+
+/**
+ * Created by Young-Ho Kim on 2016-07-19.
+ */
+class ColorPaletteView(context: Context, attrs: AttributeSet?, defStyle: Int) : RecyclerView(context, attrs, defStyle) {
+
+    private lateinit var colors : IntArray
+
+    private var selectedIndex : Int = 0
+
+    private var buttonSize : Int = 0
+
+    val colorChanged = Event<Int>()
+
+    constructor(context: Context, attrs: AttributeSet? = null) : this(context, attrs, 0)
+
+    init{
+
+        val colorStringArray = resources.getStringArray(R.array.colorPaletteArray)
+
+        colors = colorStringArray.map{ Color.parseColor(it) }.toIntArray()
+
+        buttonSize = context.resources.getDimensionPixelSize(R.dimen.color_selection_button_size)
+
+        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        adapter = Adapter()
+
+        addItemDecoration(OffsetDecoration())
+    }
+
+    fun getSelectedColor(): Int {
+        return colors[selectedIndex]
+    }
+
+    inner class Adapter() : RecyclerView.Adapter<Adapter.ViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            var view = ColorSelectionButton(context)
+            view.layoutParams = LayoutParams(buttonSize, buttonSize)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.bind(position)
+        }
+
+        override fun getItemCount(): Int {
+            return colors.count()
+        }
+
+        override fun getItemId(position: Int): Long {
+            return colors[position] as Long;
+        }
+
+
+        inner class ViewHolder(val view : View) : RecyclerView.ViewHolder(view){
+
+
+            init{
+                view.setOnClickListener {
+                    if(adapterPosition != selectedIndex)
+                    {
+                        val toDeselect = selectedIndex
+                        selectedIndex = adapterPosition
+                        notifyItemChanged(toDeselect)
+                        notifyItemChanged(selectedIndex)
+                        colorChanged.invoke(parent, selectedIndex)
+                    }
+                    else{
+                        notifyItemChanged(selectedIndex)
+                    }
+                }
+            }
+
+            fun bind(position: Int){
+                (view as ColorSelectionButton).color = colors[position]
+                (view as ColorSelectionButton).isChecked = selectedIndex == position
+            }
+        }
+    }
+
+    inner class OffsetDecoration: RecyclerView.ItemDecoration(){
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State?) {
+            super.getItemOffsets(outRect, view, parent, state)
+
+            if(colors.count() > 1) {
+
+                val margin = (parent.measuredWidth - parent.paddingLeft - parent.paddingRight - colors.count()  * buttonSize) / (colors.count()  - 1)
+
+                if (parent.getChildAdapterPosition(view) != 0) {
+                    outRect.left = margin
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/ColorSelectionButton.kt
@@ -0,0 +1,114 @@
+package kr.ac.snu.hcil.omnitrack.ui.components
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ToggleButton
+
+import kr.ac.snu.hcil.omnitrack.R
+
+/**
+ * TODO: document your custom view class.
+ */
+class ColorSelectionButton : ToggleButton {
+    var color:Int = Color.RED // TODO: use a default from R.color...
+        set(value){
+            if(field != value) {
+                field = value
+                shapePaint.color = value
+                invalidate()
+            }
+        }
+
+    /**
+     * Gets the example drawable attribute value.
+
+     * @return The example drawable attribute value.
+     */
+    /**
+     * Sets the view's example drawable attribute value. In the example view, this drawable is
+     * drawn above the text.
+
+     * @param exampleDrawable The example drawable attribute value to use.
+     */
+    lateinit var checkedDrawable: Drawable
+
+    private val frameBounds = RectF()
+    private var paddingTop = 0.0f
+    private var paddingLeft = 0.0f
+
+    private var contentSize : Float = 0.0f
+
+    val shapePaint : Paint = Paint()
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        background = null
+        // Load attributes
+        val a = context.obtainStyledAttributes(
+                attrs, R.styleable.ColorSelectionButton, defStyle, 0)
+
+        color = a.getColor(R.styleable.ColorSelectionButton_buttonColor, Color.RED)
+
+        a.recycle()
+
+        // Set up a default TextPaint object
+        shapePaint.flags = Paint.ANTI_ALIAS_FLAG
+
+        checkedDrawable = resources.getDrawable(R.drawable.done, null)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        if(changed == true)
+        {
+
+            contentSize = Math.min(width, height).toFloat()
+            if(width >= height)
+            {
+                paddingTop = 0.0f
+                paddingLeft = (width - contentSize)*.5f
+            }
+            else{
+                paddingTop = (height - contentSize)*.5f
+                paddingLeft = 0.0f
+            }
+
+            frameBounds.set(paddingLeft,paddingTop, paddingLeft +contentSize as Float, paddingTop + contentSize as Float)
+            val paddingSize = Math.round(contentSize * 0.07f)
+            val drawableSize = Math.round(paddingSize + contentSize - 2*paddingSize)
+            checkedDrawable.setBounds(Math.round(paddingLeft + paddingSize), Math.round(paddingTop + paddingSize), drawableSize, drawableSize)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if(isChecked)
+        {
+            canvas.drawRoundRect(frameBounds, contentSize*0.2f, contentSize*0.2f, shapePaint)
+            checkedDrawable.draw(canvas)
+
+        }else{
+            canvas.drawCircle(paddingLeft + contentSize*.5f, paddingTop + contentSize*.5f, contentSize*.5f, shapePaint)
+        }
+
+    }
+}
