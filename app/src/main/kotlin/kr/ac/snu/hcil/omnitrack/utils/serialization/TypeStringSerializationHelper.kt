1c932f65623f3646419f7c87ed99dd988c9789e1
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-07-28 19:36:41 +0900

    IntArray and LongArray can be serialized via TypeStringSerializer

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/serialization/TypeStringSerializationHelper.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/serialization/TypeStringSerializationHelper.kt
@@ -15,23 +15,31 @@ object TypeStringSerializationHelper {
     const val TYPENAME_BIGDECIMAL = "D"
     const val TYPENAME_TIMEPOINT = "T"
     const val TYPENAME_STRING = "S"
+    const val TYPENAME_INT_ARRAY = "I[]"
+    const val TYPENAME_LONG_ARRAY = "L[]"
 
     val classNameDictionary = mapOf(
             Int::class.java.name to TYPENAME_INT,
             Long::class.java.name to TYPENAME_LONG,
             BigDecimal::class.java.name to TYPENAME_BIGDECIMAL,
             TimePoint::class.java.name to TYPENAME_TIMEPOINT,
-            String::class.java.name to TYPENAME_STRING
+            String::class.java.name to TYPENAME_STRING,
+            IntArray::class.java.name to TYPENAME_INT_ARRAY,
+            LongArray::class.java.name to TYPENAME_LONG_ARRAY
     )
 
     private val parcelCache = ParcelWithType("", "")
     private val gson = Gson()
+
 
     fun serialize(typeName: String, value: Any): String {
         parcelCache.t = typeName
         parcelCache.v = when (parcelCache.t) {
             TYPENAME_BIGDECIMAL -> (value as BigDecimal).toPlainString()
             TYPENAME_TIMEPOINT -> (value as TimePoint).getSerializedString()
+            TYPENAME_INT_ARRAY -> (value as IntArray).joinToString(",")
+            TYPENAME_LONG_ARRAY -> (value as IntArray).joinToString(",")
+
             else -> value.toString()
         }
 
@@ -54,6 +62,9 @@ object TypeStringSerializationHelper {
             TYPENAME_STRING -> return parcel.v
             TYPENAME_BIGDECIMAL -> return BigDecimal(parcel.v)
             TYPENAME_TIMEPOINT -> return TimePoint(parcel.v)
+            TYPENAME_INT_ARRAY -> return parcel.v.split(",").map { it.toInt() }.toIntArray()
+            TYPENAME_LONG_ARRAY -> return parcel.v.split(",").map { it.toLong() }.toLongArray()
+
             else -> return parcel.v
         }
     }
