710173fd5b715048dd971343ff0c2e4e85e19757
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-08-15 16:31:32 +0900

    implented basic appearance of adjust mode

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/inputs/attributes/LocationInputView.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/inputs/attributes/LocationInputView.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewStub
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -33,10 +34,51 @@ class LocationInputView(context: Context, attrs: AttributeSet? = null) : AAttrib
 
     private val mapView: MapView
 
+    private val controlPanel: View
+
+    private var isAdjustMode: Boolean = false
+        set(value) {
+            if (field != value) {
+                field = value
+
+                if (value == true) // adjustMode
+                {
+                    controlPanel.visibility = View.GONE
+
+                    if (adjustPanel == null) {
+                        adjustPanel = inflateAdjustPanel()
+                    } else {
+                        adjustPanel?.visibility = View.VISIBLE
+                    }
+
+                    colorFrame.setBackgroundResource(R.drawable.map_view_frame_adjust_mode)
+
+                } else {
+                    controlPanel.visibility = View.VISIBLE
+
+                    if (adjustPanel != null) {
+                        adjustPanel?.visibility = View.GONE
+                    }
+
+                    colorFrame.setBackgroundResource(R.drawable.map_view_frame)
+                }
+            }
+        }
+
     private val zoomInButton: View
     private val zoomOutButton: View
+    private val adjustButton: View
+
     private val fitButton: View
 
+    private val colorFrame: View
+
+    private val adjustPanelStub: ViewStub
+
+    private var adjustPanel: View? = null
+
+    private var adjustOkButton: View? = null
+    private var adjustCancelButton: View? = null
 
 
     private var googleMap: GoogleMap? = null
@@ -47,13 +89,32 @@ class LocationInputView(context: Context, attrs: AttributeSet? = null) : AAttrib
     init {
         mapView = findViewById(R.id.ui_map) as MapView
 
+        colorFrame = findViewById(R.id.ui_mapview_frame)
+
+        controlPanel = findViewById(R.id.ui_control_panel)
+        adjustPanelStub = findViewById(R.id.ui_adjust_panel_stub) as ViewStub
+
         zoomInButton = findViewById(R.id.ui_button_zoom_in)
         zoomOutButton = findViewById(R.id.ui_button_zoom_out)
         fitButton = findViewById(R.id.ui_button_fit)
+        adjustButton = findViewById(R.id.ui_button_adjust)
 
         zoomInButton.setOnClickListener(this)
         zoomOutButton.setOnClickListener(this)
         fitButton.setOnClickListener(this)
+        adjustButton.setOnClickListener(this)
+    }
+
+    private fun inflateAdjustPanel(): View {
+        val view = adjustPanelStub.inflate()
+
+        adjustOkButton = view.findViewById(R.id.ui_button_apply)
+        adjustCancelButton = view.findViewById(R.id.ui_button_cancel)
+
+        adjustOkButton?.setOnClickListener(this)
+        adjustCancelButton?.setOnClickListener(this)
+
+        return view
     }
 
     override fun onClick(view: View) {
@@ -63,15 +124,24 @@ class LocationInputView(context: Context, attrs: AttributeSet? = null) : AAttrib
             googleMap?.animateCamera(CameraUpdateFactory.zoomBy(-1.0f))
         } else if (view === fitButton) {
             fitToValueLocation()
+        } else if (view === adjustButton) {
+            isAdjustMode = true
+        } else if (view === adjustOkButton) {
+            isAdjustMode = false
+        } else if (view === adjustCancelButton) {
+            isAdjustMode = false
         }
     }
 
     private fun fitToValueLocation() {
-        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(value, 14.0f));
-        if (marker == null) {
-            googleMap?.addMarker(MarkerOptions().position(value).draggable(false))
-        } else {
-            marker?.position = value
+        if (googleMap != null) {
+            googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(value, 14.0f));
+
+            if (marker == null) {
+                googleMap?.addMarker(MarkerOptions().position(value).draggable(false))
+            } else {
+                marker?.position = value
+            }
         }
     }
 
@@ -94,6 +164,9 @@ class LocationInputView(context: Context, attrs: AttributeSet? = null) : AAttrib
 
     override fun onMapReady(map: GoogleMap?) {
         this.googleMap = map
+        this.googleMap?.setMinZoomPreference(3.0f)
+        this.googleMap?.setMaxZoomPreference(17.0f)
+
     }
 
 
