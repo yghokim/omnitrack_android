79da7093260cf613d031b530cb783b6e7de9f6e1
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-08-24 00:17:20 +0900

    added trigger type selection dialog

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/tracker/TrackerDetailTriggerTabFragment.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/tracker/TrackerDetailTriggerTabFragment.kt
@@ -1,6 +1,8 @@
 package kr.ac.snu.hcil.omnitrack.ui.pages.tracker
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -12,6 +14,7 @@ import kr.ac.snu.hcil.omnitrack.OTApplication
 import kr.ac.snu.hcil.omnitrack.R
 import kr.ac.snu.hcil.omnitrack.core.triggers.OTTrigger
 import kr.ac.snu.hcil.omnitrack.ui.components.decorations.HorizontalDividerItemDecoration
+import kr.ac.snu.hcil.omnitrack.ui.pages.trigger.NewTriggerTypeSelectionDialogHelper
 
 /**
  * Created by younghokim on 16. 7. 30..
@@ -22,6 +25,16 @@ class TrackerDetailTriggerTabFragment : TrackerDetailActivity.ChildFragment() {
 
     private lateinit var listView: RecyclerView
 
+    private lateinit var newTriggerButton: FloatingActionButton
+
+    private val triggerTypeDialog: AlertDialog by lazy {
+        NewTriggerTypeSelectionDialogHelper.builder(context) {
+            type ->
+            println("trigger type selected - $type")
+            triggerTypeDialog.dismiss()
+        }.create()
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView = inflater!!.inflate(R.layout.fragment_tracker_detail_triggers, container, false)
 
@@ -31,6 +44,13 @@ class TrackerDetailTriggerTabFragment : TrackerDetailActivity.ChildFragment() {
         adapter = Adapter()
         listView.adapter = adapter
         listView.addItemDecoration(HorizontalDividerItemDecoration(resources.getColor(R.color.dividerColor, null), resources.getDimensionPixelSize(R.dimen.trigger_list_element_divider_height)))
+
+
+        newTriggerButton = rootView.findViewById(R.id.ui_button_new_trigger) as FloatingActionButton
+
+        newTriggerButton.setOnClickListener {
+            triggerTypeDialog.show()
+        }
 
         return rootView
     }
---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/trigger/NewTriggerTypeSelectionDialog.kt
@@ -0,0 +1,77 @@
+package kr.ac.snu.hcil.omnitrack.ui.pages.trigger
+
+import android.content.Context
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kr.ac.snu.hcil.omnitrack.R
+import kr.ac.snu.hcil.omnitrack.core.triggers.OTTrigger
+
+/**
+ * Created by younghokim on 16. 8. 23..
+ */
+object NewTriggerTypeSelectionDialogHelper {
+
+    data class TriggerTypeEntry(val typeCode: Int, val iconId: Int, val nameId: Int, val descId: Int)
+
+    val triggerTypes = arrayOf(
+            TriggerTypeEntry(OTTrigger.TYPE_TIME, R.drawable.alarm_dark, R.string.trigger_name_time, R.string.trigger_desc_time),
+            TriggerTypeEntry(OTTrigger.TYPE_SERVICE_EVENT, R.drawable.event_dark, R.string.trigger_name_event, R.string.trigger_desc_event)
+    )
+
+    fun builder(context: Context, listener: (Int) -> Unit): AlertDialog.Builder {
+
+        val view = LayoutInflater.from(context).inflate(R.layout.simple_layout_with_recycler_view, null)
+        val listView = view.findViewById(R.id.ui_list) as RecyclerView
+
+        listView.layoutManager = GridLayoutManager(context, 2)
+        listView.adapter = Adapter(listener)
+
+        return AlertDialog.Builder(context)
+                .setTitle(context.resources.getString(R.string.msg_select_trigger_type))
+                .setView(view)
+    }
+
+    private class Adapter(val listener: (Int) -> Unit) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+        override fun getItemCount(): Int {
+            return triggerTypes.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.iconView.setImageResource(triggerTypes[position].iconId)
+            holder.nameView.setText(triggerTypes[position].nameId)
+            holder.descView.setText(triggerTypes[position].descId)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_trigger_type_selection_element, null)
+            return ViewHolder(view)
+        }
+
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+            val iconView: AppCompatImageView
+            val nameView: TextView
+            val descView: TextView
+
+            init {
+                iconView = view.findViewById(R.id.ui_icon) as AppCompatImageView
+                nameView = view.findViewById(R.id.ui_name) as TextView
+                descView = view.findViewById(R.id.ui_description) as TextView
+                view.setOnClickListener(this)
+            }
+
+            override fun onClick(p0: View?) {
+                listener.invoke(triggerTypes[adapterPosition].typeCode)
+            }
+        }
+
+    }
+}
