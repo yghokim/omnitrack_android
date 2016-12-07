6fb6536c1217e5339b3294b307e9a331a031b2cd
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-12-07 21:09:18 +0900

    TextView Clear focus and hide keyboard when touched outside and enter key pressed

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/activities/OTActivity.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/activities/OTActivity.kt
@@ -1,6 +1,11 @@
 package kr.ac.snu.hcil.omnitrack.ui.activities
 
+import android.content.Context
+import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.google.gson.JsonObject
 import kr.ac.snu.hcil.omnitrack.OTApplication
 
@@ -37,5 +42,21 @@ abstract class OTActivity : AppCompatActivity() {
     }
 
     protected open fun onSessionLogContent(contentObject: JsonObject) {
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
