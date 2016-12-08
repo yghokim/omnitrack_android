c98505fcddfcb03d5a4d9c79dbb8df2eb5c1a88f
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-12-08 16:44:28 +0900

    revised duration picker interaction on trigger

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/activities/OTActivity.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/activities/OTActivity.kt
@@ -1,6 +1,7 @@
 package kr.ac.snu.hcil.omnitrack.ui.activities
 
 import android.content.Context
+import android.graphics.PointF
 import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
@@ -8,6 +9,8 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import com.google.gson.JsonObject
 import kr.ac.snu.hcil.omnitrack.OTApplication
+import kr.ac.snu.hcil.omnitrack.ui.components.common.time.DurationPicker
+import java.util.*
 
 /**
  * Created by younghokim on 2016. 11. 15..
@@ -18,10 +21,19 @@ abstract class OTActivity : AppCompatActivity() {
 
     private var resumedAt: Long = 0
 
+    val durationPickers = ArrayList<DurationPicker>()
+
+    private var touchMoveAmount: PointF = PointF()
+
     override fun onResume() {
         super.onResume()
 
         resumedAt = System.currentTimeMillis()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        durationPickers.clear()
     }
 
     override fun onPause() {
@@ -46,6 +58,7 @@ abstract class OTActivity : AppCompatActivity() {
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_DOWN) {
+
             val v = currentFocus
             if (v is EditText) {
                 val outRect = Rect()
@@ -56,7 +69,23 @@ abstract class OTActivity : AppCompatActivity() {
                     imm.hideSoftInputFromWindow(v.windowToken, 0)
                 }
             }
+        } else if (event.action == MotionEvent.ACTION_MOVE) {
+            touchMoveAmount.x++
+            touchMoveAmount.y++
+        } else if (event.action == MotionEvent.ACTION_UP) {
+            if (touchMoveAmount.x < 10 || touchMoveAmount.y < 10) {
+                for (v in durationPickers) {
+                    val outRect = Rect()
+                    v.getGlobalVisibleRect(outRect)
+                    if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                        v.setInputMode(false, true)
+                    }
+                }
+            }
+            touchMoveAmount.x = 0f
+            touchMoveAmount.y = 0f
         }
+
         return super.dispatchTouchEvent(event)
     }
 }
