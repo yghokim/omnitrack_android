7d6efe4f0206a7d1b6022de115826bf94153d70b
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-12-19 23:52:03 +0900

    refactoring DataTriggerManager. Employed GcmService for periodic check.

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/services/DataTriggerCheckService.kt
@@ -0,0 +1,36 @@
+package kr.ac.snu.hcil.omnitrack.services
+
+import com.google.android.gms.gcm.*
+import kr.ac.snu.hcil.omnitrack.OTApplication
+import kr.ac.snu.hcil.omnitrack.core.triggers.OTDataTriggerManager
+
+class DataTriggerCheckService : GcmTaskService() {
+
+    companion object {
+        const val TAG_DATA_TRIGGER_CHECK = "dataTriggerCompare"
+
+        fun makeTask(periodSecond: Long): PeriodicTask {
+            return PeriodicTask.Builder()
+                    .setService(DataTriggerCheckService::class.java)
+                    .setPeriod(periodSecond)
+                    .setRequiredNetwork(Task.NETWORK_STATE_CONNECTED)
+                    .setTag(TAG_DATA_TRIGGER_CHECK)
+                    .setPersisted(true)
+                    .setRequiresCharging(false)
+                    .build()
+        }
+    }
+
+    override fun onRunTask(params: TaskParams): Int {
+
+        when (params.tag) {
+            TAG_DATA_TRIGGER_CHECK -> {
+                OTDataTriggerManager.checkMeasures(OTApplication.app)
+
+                return GcmNetworkManager.RESULT_SUCCESS
+            }
+            else -> return GcmNetworkManager.RESULT_FAILURE
+        }
+    }
+
+}
