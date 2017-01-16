34a63085b246b84b030987714f21b6545ce285dc
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-01-16 16:21:08 +0900

    implementing compounddrawable tint

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/DrawableTintAppCompatTextView.kt
@@ -0,0 +1,25 @@
+package kr.ac.snu.hcil.omnitrack.ui.components.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+
+/**
+ * Created by younghokim on 2017. 1. 13..
+ */
+class DrawableTintAppCompatTextView : TextView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val core = CompoundDrawableTintCore()
+        val cd = core.init(context, compoundDrawables, attrs, 0)
+        setCompoundDrawables(cd[0], cd[1], cd[2], cd[3])
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        val core = CompoundDrawableTintCore()
+        val cd = core.init(context, compoundDrawables, attrs, 0)
+        setCompoundDrawables(cd[0], cd[1], cd[2], cd[3])
+    }
+}
