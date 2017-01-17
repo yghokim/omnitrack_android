d6703ff9a32e422d745eacae6c042deec8377177
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-01-17 17:09:26 +0900

    Implemented ExtendedSpinner that consists of ViewGroup and removed layer-list background.

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/DrawableTintAppCompatTextView.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/DrawableTintAppCompatTextView.kt
@@ -7,19 +7,19 @@ import android.widget.TextView
 /**
  * Created by younghokim on 2017. 1. 13..
  */
-class DrawableTintAppCompatTextView : TextView {
+open class DrawableTintAppCompatTextView : TextView {
 
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val core = CompoundDrawableTintCore()
-        val cd = core.init(this, context, compoundDrawablesRelative, attrs, 0)
-        setCompoundDrawablesRelativeWithIntrinsicBounds(cd[0], cd[1], cd[2], cd[3])
+        val cd = core.init(context, compoundDrawablesRelative, attrs, 0)
+        super.setCompoundDrawablesRelativeWithIntrinsicBounds(cd[0], cd[1], cd[2], cd[3])
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val core = CompoundDrawableTintCore()
-        val cd = core.init(this, context, compoundDrawablesRelative, attrs, 0)
-        setCompoundDrawablesRelativeWithIntrinsicBounds(cd[0], cd[1], cd[2], cd[3])
+        val cd = core.init(context, compoundDrawablesRelative, attrs, 0)
+        super.setCompoundDrawablesRelativeWithIntrinsicBounds(cd[0], cd[1], cd[2], cd[3])
     }
 }
