3a087aef33d85889f6749c600652ba75e5c9e120
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-02-02 18:02:32 +0900

    optimized sign-in process that network sign-in will be ignored on other than splashscreen

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/SplashScreenActivity.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/SplashScreenActivity.kt
@@ -9,7 +9,7 @@ import kr.ac.snu.hcil.omnitrack.ui.pages.home.HomeActivity
  * Created by Young-Ho on 8/1/2016.
  * https://www.bignerdranch.com/blog/splash-screens-the-right-way/
  */
-class SplashScreenActivity : OTActivity() {
+class SplashScreenActivity : OTActivity(checkRefreshingCredential = true) {
 
     private val LOG_TAG = SplashScreenActivity::class.java.simpleName
 
