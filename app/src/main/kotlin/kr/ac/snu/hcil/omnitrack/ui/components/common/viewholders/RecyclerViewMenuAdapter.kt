7b177b022d67ffc1eecb8a19673a40bdcd3b3eef
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-07 11:07:56 +0900

    added interface of settings bottomsheet to item browser activity

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/viewholders/RecyclerViewMenuAdapter.kt
@@ -0,0 +1,66 @@
+package kr.ac.snu.hcil.omnitrack.ui.components.common.viewholders
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import kr.ac.snu.hcil.omnitrack.R
+import kr.ac.snu.hcil.omnitrack.utils.inflateContent
+
+/**
+ * Created by younghokim on 2017. 3. 6..
+ */
+abstract class RecyclerViewMenuAdapter : RecyclerView.Adapter<RecyclerViewMenuAdapter.MenuViewHolder>() {
+    abstract fun getMenuItemAt(index: Int): MenuItem
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.bind(getMenuItemAt(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        return MenuViewHolder(parent.inflateContent(R.layout.simple_menu_element_with_icon_title_description, false))
+    }
+
+    data class MenuItem(var iconRes: Int?, var name: String?, var description: String?, var onClick: () -> Unit, var isEnabled: Boolean = true)
+
+    class MenuViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        var menuItem: MenuItem? = null
+        val wrapped = SimpleIconNameDescriptionViewHolder(view)
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(clicked: View) {
+            if (clicked === this.itemView && this.menuItem?.isEnabled == true) {
+                menuItem?.onClick?.invoke()
+            }
+        }
+
+
+        fun bind(item: MenuItem) {
+            menuItem = item
+            if (item.iconRes == null) {
+                wrapped.iconView.visibility = View.GONE
+            } else {
+                wrapped.iconView.visibility = View.VISIBLE
+                wrapped.setIconDrawable(item.iconRes!!)
+            }
+
+            if (item.description == null) {
+                wrapped.descriptionView.visibility = View.GONE
+            } else {
+                wrapped.descriptionView.visibility = View.VISIBLE
+                wrapped.descriptionView.text = item.description
+            }
+
+            wrapped.nameView.text = item.name
+
+            if (item.isEnabled) {
+                itemView.alpha = 1f
+            } else {
+                itemView.alpha = 0.2f
+            }
+        }
+    }
+}
