c4875802cdaa7d0ca7ff372e9ebf0bda5d68e8d0
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-08 22:00:51 +0900

    added loading indicator bar shown during ItemBuilder working.

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/LoadingIndicatorBar.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/LoadingIndicatorBar.kt
@@ -1,10 +1,15 @@
 package kr.ac.snu.hcil.omnitrack.ui.components.common
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.FrameLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.github.ybq.android.spinkit.SpinKitView
 import kr.ac.snu.hcil.omnitrack.R
 import kr.ac.snu.hcil.omnitrack.utils.inflateContent
 
@@ -13,8 +18,36 @@ import kr.ac.snu.hcil.omnitrack.utils.inflateContent
  */
 class LoadingIndicatorBar : FrameLayout {
 
-    private val progressBar: ProgressBar
+    private val progressBar: SpinKitView
     private val messageView: TextView
+
+    private val showAnimator: ValueAnimator
+    private val dismissAnimator: ValueAnimator
+    private val animationUpdateListener: ValueAnimator.AnimatorUpdateListener
+
+
+    private var isIndicatorShown: Boolean = false
+        private set(value)
+        {
+            if(value!=field) {
+                if (value == true) {
+                    this.layoutParams?.height = expandedHeight
+                } else {
+                    this.layoutParams?.height = 0
+                }
+
+                field = value
+            }
+        }
+
+    private var message: CharSequence get()=messageView.text.toString()
+        set(value){
+            messageView.text = value
+        }
+
+    private val expandedHeight: Int by lazy{
+        context.resources.getDimensionPixelSize(R.dimen.loading_indicator_bar_height)
+    }
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
@@ -23,8 +56,94 @@ class LoadingIndicatorBar : FrameLayout {
 
     init {
         inflateContent(R.layout.component_loading_indicator_bar, true)
-        progressBar = findViewById(R.id.ui_progress_bar) as ProgressBar
+        progressBar = findViewById(R.id.ui_progress_bar) as SpinKitView
         messageView = findViewById(R.id.ui_message) as TextView
 
+        animationUpdateListener = ValueAnimator.AnimatorUpdateListener {
+            animator->
+            val heightValue = animator.animatedValue as Float
+            this@LoadingIndicatorBar.layoutParams?.height = (heightValue+.5f).toInt()
+            this@LoadingIndicatorBar.requestLayout()
+        }
+
+        showAnimator = ValueAnimator.ofFloat(0f, expandedHeight.toFloat()).apply{
+            duration = 250
+            interpolator = DecelerateInterpolator()
+
+            addUpdateListener(animationUpdateListener)
+            addListener(object: Animator.AnimatorListener {
+                override fun onAnimationEnd(p0: Animator?) {
+
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                    this@LoadingIndicatorBar.visibility = View.INVISIBLE
+                }
+
+                override fun onAnimationStart(p0: Animator?) {
+                    this@LoadingIndicatorBar.visibility = View.VISIBLE
+                    this@LoadingIndicatorBar.layoutParams?.height = 0
+                    isIndicatorShown = true
+                }
+
+                override fun onAnimationRepeat(p0: Animator?) {
+
+                }
+
+            })
+        }
+
+        dismissAnimator = ValueAnimator.ofFloat(expandedHeight.toFloat(), 0f).apply{
+            duration = 400
+            interpolator = DecelerateInterpolator()
+
+            addUpdateListener(animationUpdateListener)
+            addListener(object: Animator.AnimatorListener {
+                override fun onAnimationEnd(p0: Animator?) {
+                    this@LoadingIndicatorBar.visibility = View.INVISIBLE
+                    this@LoadingIndicatorBar.layoutParams?.height = 0
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                    this@LoadingIndicatorBar.visibility = View.VISIBLE
+                    this@LoadingIndicatorBar.layoutParams?.height = expandedHeight
+                }
+
+                override fun onAnimationStart(p0: Animator?) {
+                    this@LoadingIndicatorBar.layoutParams?.height = expandedHeight
+                    isIndicatorShown = false
+                }
+
+                override fun onAnimationRepeat(p0: Animator?) {
+
+                }
+
+            })
+        }
     }
+
+    fun setMessage(res: Int){
+        this.messageView.setText(res)
+    }
+
+    fun show(){
+        if(!isIndicatorShown) {
+            if (dismissAnimator.isRunning) {
+                dismissAnimator.cancel()
+            }
+
+            showAnimator.start()
+        }
+    }
+
+    fun dismiss(){
+        if(isIndicatorShown) {
+            if (showAnimator.isRunning) {
+                showAnimator.cancel()
+            }
+
+            dismissAnimator.start()
+        }
+    }
+
 }
