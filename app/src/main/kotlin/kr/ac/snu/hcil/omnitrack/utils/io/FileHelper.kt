28ae99e91754bdfbb4716b2daee1edd4371eb401
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-09 15:23:08 +0900

    implementing Table export

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/io/FileHelper.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/io/FileHelper.kt
@@ -1,5 +1,6 @@
 package kr.ac.snu.hcil.omnitrack.utils.io
 
+import android.content.Intent
 import com.koushikdutta.async.util.FileUtility
 import rx.Single
 import java.io.File
@@ -10,5 +11,13 @@ import java.io.File
 object FileHelper {
     fun removeAllFilesIn(dir: File): Single<Boolean> {
         return Single.just(FileUtility.deleteDirectory(dir))
+    }
+
+    fun makeSaveLocationPickIntent(filename: String, mimeType: String): Intent {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.setType("application/zip")
+
+        return intent
     }
 }
