fa5af31c8bf987a5824efb22d4d1af150476c2f4
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-09 15:20:55 +0900

    implementing table export

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/services/OTTableExportService.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/services/OTTableExportService.kt
@@ -1,24 +1,41 @@
 package kr.ac.snu.hcil.omnitrack.services
 
 import android.app.IntentService
+import android.content.Context
 import android.content.Intent
 import kr.ac.snu.hcil.omnitrack.OTApplication
+import kr.ac.snu.hcil.omnitrack.core.OTTracker
 import kr.ac.snu.hcil.omnitrack.core.database.FirebaseDbHelper
 import kr.ac.snu.hcil.omnitrack.utils.io.StringTableSheet
 import rx.Observable
 import rx.internal.util.SubscriptionList
+import java.io.File
 
 /**
  * Created by Young-Ho on 3/9/2017.
  */
 class OTTableExportService : IntentService("Table Export Service") {
+
+    companion object {
+        const val EXTRA_EXPORT_URI = "export_uri"
+
+        fun makeIntent(context: Context, tracker: OTTracker, exportUri: String): Intent {
+            val intent = Intent(context, OTTableExportService::class.java)
+                    .putExtra(OTApplication.INTENT_EXTRA_OBJECT_ID_TRACKER, tracker.objectId)
+                    .putExtra(EXTRA_EXPORT_URI, exportUri)
+            return intent
+        }
+
+    }
+
     private val subscriptions = SubscriptionList()
 
     override fun onHandleIntent(intent: Intent) {
 
         println("table export service started.")
         val trackerId = intent.getStringExtra(OTApplication.INTENT_EXTRA_OBJECT_ID_TRACKER)
-        if (trackerId != null) {
+        val exportUri = intent.getStringExtra(EXTRA_EXPORT_URI)
+        if (trackerId != null && exportUri != null) {
 
             val table = OTApplication.app.currentUserObservable
                     .flatMap {
@@ -26,6 +43,14 @@ class OTTableExportService : IntentService("Table Export Service") {
                         val tracker = user[trackerId]
                         if (tracker != null) {
                             val table = StringTableSheet()
+
+                            val externalFilesInvolved = tracker.attributes.unObservedList.find { it.isExternalFile } != null
+
+                            var cacheDir: File? = null
+                            if (externalFilesInvolved) {
+                                cacheDir = this.cacheDir.resolve("export_${System.currentTimeMillis()}")
+                            }
+
                             tracker.attributes.unObservedList.forEach {
                                 it.onAddColumnToTable(table.columns)
                             }
