ee9572043c7d5d44392c05feb9f97d6e6e75098e
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-10 18:04:19 +0900

    show warning message on trying export via Mobile Data .

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/net/NetworkHelper.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/net/NetworkHelper.kt
@@ -8,6 +8,11 @@ import kr.ac.snu.hcil.omnitrack.OTApplication
  * Created by younghokim on 2016. 10. 25..
  */
 object NetworkHelper {
+
+    data class NetworkConnectionInfo(val wifiConnected: Boolean, val mobileConnected: Boolean) {
+        val isInternetConnected: Boolean get() = wifiConnected || mobileConnected
+    }
+
     fun isConnectedToInternet(): Boolean {
         val connectivityManager = OTApplication.app.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
@@ -17,5 +22,15 @@ object NetworkHelper {
             OTApplication.logger.writeSystemLog("internet connection: wifi- $wifi, mobile- $mobile", "NetworkHelper")
             return wifi || mobile
         } else return false
+    }
+
+    fun getCurrentNetworkConnectionInfo(): NetworkConnectionInfo {
+        val connectivityManager = OTApplication.app.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null) {
+            val wifi = networkInfo.type == ConnectivityManager.TYPE_WIFI
+            val mobile = networkInfo.type == ConnectivityManager.TYPE_MOBILE
+            return NetworkConnectionInfo(wifi, mobile)
+        } else return NetworkConnectionInfo(false, false)
     }
 }
