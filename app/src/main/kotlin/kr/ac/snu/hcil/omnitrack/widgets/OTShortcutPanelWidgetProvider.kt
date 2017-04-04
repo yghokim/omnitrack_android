3708e61bb292eb55260e8b5b5618af49935e2d31
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-04-04 12:42:27 +0900

    extracted widget update logic to service widget receives sign in change broadcast and change its mode.

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/widgets/OTShortcutPanelWidgetProvider.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/widgets/OTShortcutPanelWidgetProvider.kt
@@ -1,49 +1,43 @@
 package kr.ac.snu.hcil.omnitrack.widgets
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.view.View
-import android.widget.RemoteViews
-import kr.ac.snu.hcil.omnitrack.R
-import kr.ac.snu.hcil.omnitrack.core.backend.OTAuthManager
-import kr.ac.snu.hcil.omnitrack.ui.pages.SignInActivity
+import kr.ac.snu.hcil.omnitrack.OTApplication
 
 /**
  * Created by younghokim on 2017. 3. 30..
  */
 class OTShortcutPanelWidgetProvider : AppWidgetProvider() {
 
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        if (intent.action == OTApplication.BROADCAST_ACTION_USER_SIGNED_IN || intent.action == OTApplication.BROADCAST_ACTION_USER_SIGNED_OUT) {
+            val updateIntent = Intent(context, OTShortcutPanelWidgetUpdateService::class.java)
+
+            updateIntent.action = when (intent.action) {
+                OTApplication.BROADCAST_ACTION_USER_SIGNED_IN -> OTShortcutPanelWidgetUpdateService.ACTION_TO_MAIN_MODE
+                OTApplication.BROADCAST_ACTION_USER_SIGNED_OUT -> OTShortcutPanelWidgetUpdateService.ACTION_TO_SIGN_IN_MODE
+                else -> OTShortcutPanelWidgetUpdateService.ACTION_TO_SIGN_IN_MODE
+            }
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+
+            updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetManager.getAppWidgetIds(ComponentName(context, OTShortcutPanelWidgetProvider::class.java)))
+
+            context.startService(updateIntent)
+        }
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
 
-        if (OTAuthManager.currentSignedInLevel > OTAuthManager.SignedInLevel.NONE) {
+        val intent = Intent(context, OTShortcutPanelWidgetUpdateService::class.java)
+                .putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+        intent.action = OTShortcutPanelWidgetUpdateService.ACTION_INITIALIZE
 
-            for (id in appWidgetIds) {
-                val rv = RemoteViews(context.packageName, R.layout.remoteview_widget_shortcut_body)
-                rv.setViewVisibility(R.id.ui_progress_bar, View.INVISIBLE)
-
-                val intent = Intent(context, OTShortcutPanelWidgetService::class.java)
-                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id)
-
-                rv.setRemoteAdapter(R.id.ui_list, intent)
-
-                appWidgetManager.updateAppWidget(id, rv)
-            }
-        } else {
-            for (id in appWidgetIds) {
-                val rv = RemoteViews(context.packageName, R.layout.remoteview_widget_login)
-
-                val signInIntent = PendingIntent.getActivity(context, 0,
-                        Intent(context, SignInActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK),
-                        PendingIntent.FLAG_CANCEL_CURRENT)
-
-                rv.setOnClickPendingIntent(R.id.ui_button_signin, signInIntent)
-
-                appWidgetManager.updateAppWidget(id, rv)
-            }
-        }
+        context.startService(intent)
 
         super.onUpdate(context, appWidgetManager, appWidgetIds)
     }
