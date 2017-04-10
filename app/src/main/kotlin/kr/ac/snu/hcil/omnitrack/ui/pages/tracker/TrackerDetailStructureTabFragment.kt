39bb6e7f29215e8617b53910a1ef37c031e6e993
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-04-10 11:22:37 +0900

    fixed material dialog xml crash bug replaced recyclerview in attribute list with linearlayout, fixed animation bugs

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/tracker/TrackerDetailStructureTabFragment.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/tracker/TrackerDetailStructureTabFragment.kt
@@ -11,7 +11,6 @@ import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
 import android.text.InputType
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
@@ -19,7 +18,6 @@ import android.widget.TextView
 import android.widget.Toast
 import butterknife.bindView
 import com.afollestad.materialdialogs.MaterialDialog
-import jp.wasabeef.recyclerview.animators.SlideInRightAnimator
 import kr.ac.snu.hcil.omnitrack.OTApplication
 import kr.ac.snu.hcil.omnitrack.R
 import kr.ac.snu.hcil.omnitrack.core.OTTracker
@@ -27,10 +25,8 @@ import kr.ac.snu.hcil.omnitrack.core.OTUser
 import kr.ac.snu.hcil.omnitrack.core.attributes.AttributePresetInfo
 import kr.ac.snu.hcil.omnitrack.core.attributes.OTAttribute
 import kr.ac.snu.hcil.omnitrack.core.database.EventLoggingManager
-import kr.ac.snu.hcil.omnitrack.ui.DragItemTouchHelperCallback
-import kr.ac.snu.hcil.omnitrack.ui.components.common.FallbackRecyclerView
+import kr.ac.snu.hcil.omnitrack.ui.components.common.AdapterLinearLayout
 import kr.ac.snu.hcil.omnitrack.ui.components.common.LockableFrameLayout
-import kr.ac.snu.hcil.omnitrack.ui.components.decorations.SpaceItemDecoration
 import kr.ac.snu.hcil.omnitrack.ui.components.inputs.attributes.AAttributeInputView
 import kr.ac.snu.hcil.omnitrack.ui.components.inputs.properties.BooleanPropertyView
 import kr.ac.snu.hcil.omnitrack.ui.components.inputs.properties.ColorPalettePropertyView
@@ -38,6 +34,7 @@ import kr.ac.snu.hcil.omnitrack.ui.components.inputs.properties.ShortTextPropert
 import kr.ac.snu.hcil.omnitrack.ui.components.tutorial.TutorialManager
 import kr.ac.snu.hcil.omnitrack.ui.pages.ConnectionIndicatorStubProxy
 import kr.ac.snu.hcil.omnitrack.ui.pages.attribute.AttributeDetailActivity
+import kr.ac.snu.hcil.omnitrack.utils.DialogHelper
 import kr.ac.snu.hcil.omnitrack.utils.startActivityOnDelay
 import net.i2p.android.ext.floatingactionbutton.FloatingActionButton
 import rx.internal.util.SubscriptionList
@@ -54,7 +51,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
 
     lateinit private var rootScrollView: NestedScrollView
 
-    lateinit private var attributeListView: FallbackRecyclerView
+    lateinit private var attributeListView: AdapterLinearLayout
     lateinit private var attributeListAdapter: AttributeListAdapter
 
     lateinit private var attributeListItemTouchHelper: ItemTouchHelper
@@ -129,7 +126,8 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             }
         }
 
-        attributeListView = rootView.findViewById(R.id.ui_attribute_list) as FallbackRecyclerView
+        attributeListView = rootView.findViewById(R.id.ui_attribute_list) as AdapterLinearLayout
+        attributeListView.setViewIntervalDistance((resources.displayMetrics.density * 8 + .5f).toInt())
         attributeListView.emptyView = rootView.findViewById(R.id.ui_empty_list_message)
         val layoutManager = object : LinearLayoutManager(context, VERTICAL, false) {
             override fun canScrollVertically(): Boolean {
@@ -149,12 +147,8 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             }
         }
 
-        attributeListView.layoutManager = layoutManager
-        attributeListView.itemAnimator = SlideInRightAnimator()
-        attributeListView.addItemDecoration(SpaceItemDecoration(LinearLayoutManager.VERTICAL, resources.getDimensionPixelOffset(R.dimen.attribute_list_element_vertical_space)))
-
         //val snackBarContainer: CoordinatorLayout = rootView.findViewById(R.id.ui_snackbar_container) as CoordinatorLayout
-        removalSnackbar = Snackbar.make(rootView, resources.getText(R.string.msg_attribute_removed_message), Snackbar.LENGTH_LONG)
+        removalSnackbar = Snackbar.make(rootView, resources.getText(R.string.msg_attribute_removed_message), 5000)
         removalSnackbar.setAction(resources.getText(R.string.msg_undo)) {
             view ->
             attributeListAdapter.undoRemove()
@@ -180,10 +174,13 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
 
         attributeListAdapter = AttributeListAdapter()
 
-        attributeListView.adapter = attributeListAdapter
+        attributeListView.setOnViewSwapListener { firstView, firstPosition, secondView, secondPosition ->
+            attributeListAdapter.onMoveItem(firstPosition, secondPosition)
+        }
 
-        attributeListItemTouchHelper = ItemTouchHelper(DragItemTouchHelperCallback(attributeListAdapter, context, true, false))
-        attributeListItemTouchHelper.attachToRecyclerView(attributeListView)
+        attributeListView.adapter = attributeListAdapter as AdapterLinearLayout.ViewHolderAdapter<AdapterLinearLayout.AViewHolder>
+        //attributeListItemTouchHelper = ItemTouchHelper(DragItemTouchHelperCallback(attributeListAdapter, context, true, false))
+        //attributeListItemTouchHelper.attachToRecyclerView(attributeListView)
     }
 
     fun refresh() {
@@ -270,6 +267,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
         }
     }
 
+
     fun showRemovalSnackbar() {
         if (!removalSnackbar.isShown) {
             removalSnackbar.show()
@@ -286,7 +284,29 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
     }
 
 
-    inner class AttributeListAdapter() : RecyclerView.Adapter<AttributeListAdapter.ViewHolder>(), DragItemTouchHelperCallback.ItemDragHelperAdapter {
+    inner class AttributeListAdapter() : AdapterLinearLayout.ViewHolderAdapter<AttributeListAdapter.ViewHolder>() {
+
+
+        override fun getViewTypeCount(): Int {
+            return 1
+        }
+
+        override fun isEmpty(): Boolean {
+            return tracker?.attributes?.size ?: 0 > 0
+        }
+
+        override fun getItem(position: Int): Any {
+            return tracker?.attributes?.get(position) as Any
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return 0
+        }
+
+
+        override fun hasStableIds(): Boolean {
+            return false
+        }
 
         private var removed: OTAttribute<out Any>? = null
         private var removedPosition: Int = -1
@@ -303,7 +323,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             holder.bindAttribute(tracker!!.attributes[position])
         }
 
-        override fun getItemCount(): Int {
+        override fun getCount(): Int {
             return tracker?.attributes?.size ?: 0
         }
 
@@ -314,7 +334,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
         fun undoRemove() {
             if (removed != null) {
                 tracker?.attributes?.addAt(removed!!, removedPosition)
-                notifyItemInserted(removedPosition)
+                //notifyItemInserted(removedPosition)
             }
         }
 
@@ -322,9 +342,9 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             removed = null
         }
 
-        override fun onMoveItem(fromPosition: Int, toPosition: Int) {
+        fun onMoveItem(fromPosition: Int, toPosition: Int) {
             tracker?.attributes?.moveItem(fromPosition, toPosition)
-            notifyItemMoved(fromPosition, toPosition)
+            //notifyItemMoved(fromPosition, toPosition)
         }
 
         /*
@@ -342,11 +362,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             return null
         }*/
 
-        override fun onRemoveItem(position: Int) {
-        }
-
-
-        inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view), View.OnClickListener, View.OnTouchListener {
+        inner class ViewHolder(val view: View) : AdapterLinearLayout.AViewHolder(view), View.OnClickListener {
 
             private val previewContainer: LockableFrameLayout by bindView(R.id.ui_preview_container)
             private val columnNameView: TextView by bindView(R.id.ui_column_name)
@@ -356,8 +372,6 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             private val removeButton: ImageButton by bindView(R.id.ui_button_remove)
 
             private val requiredMarker: View by bindView(R.id.ui_required_marker)
-
-            private val draggableZone: View by bindView(R.id.ui_drag_handle)
 
             private val columnNameButton: View by bindView(R.id.ui_column_name_button)
 
@@ -389,7 +403,6 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
 
                 editButton.setOnClickListener(this)
                 removeButton.setOnClickListener(this)
-                draggableZone.setOnTouchListener(this)
                 columnNameButton.setOnClickListener(this)
 
                 columnNameChangeDialog = MaterialDialog.Builder(this.view.context)
@@ -408,14 +421,20 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
                 } else if (view === removeButton) {
                     val tracker = tracker
                     tracker?.let {
-                        removed = tracker.attributes[adapterPosition]
-                        removedPosition = adapterPosition
-                        tracker.attributes.remove(tracker.attributes[adapterPosition])
-                        notifyItemRemoved(adapterPosition)
-                        showRemovalSnackbar()
-                        if (removed != null) {
-                            EventLoggingManager.logAttributeChangeEvent(EventLoggingManager.EVENT_NAME_CHANGE_ATTRIBUTE_REMOVE, removed!!.typeId, removed!!.objectId, tracker.objectId)
-                        }
+                        DialogHelper.makeNegativePhrasedYesNoDialogBuilder(this@TrackerDetailStructureTabFragment.context,
+                                getString(R.string.msg_remove_field),
+                                String.format(getString(R.string.msg_format_confirm_remove_field),
+                                        tracker.attributes[adapterPosition].name
+                                ),
+                                R.string.msg_remove, R.string.msg_cancel, {
+                            removed = tracker.attributes[adapterPosition]
+                            removedPosition = adapterPosition
+                            tracker.attributes.remove(tracker.attributes[adapterPosition])
+                            showRemovalSnackbar()
+                            if (removed != null) {
+                                EventLoggingManager.logAttributeChangeEvent(EventLoggingManager.EVENT_NAME_CHANGE_ATTRIBUTE_REMOVE, removed!!.typeId, removed!!.objectId, tracker.objectId)
+                            }
+                        }, onNo = null).show()
                     }
                 } else if (view === columnNameButton) {
                     val tracker = tracker
@@ -430,14 +449,6 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
                                 }.show()
                     }
                 }
-            }
-
-            override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
-
-                if (motionEvent.action == MotionEvent.ACTION_DOWN) {
-                    attributeListItemTouchHelper.startDrag(this@ViewHolder)
-                    return true
-                } else return false
             }
 
             fun bindAttribute(attribute: OTAttribute<out Any>) {
@@ -479,7 +490,7 @@ class TrackerDetailStructureTabFragment : TrackerDetailActivity.ChildFragment() 
             tracker.attributes.add(newAttribute)
             EventLoggingManager.logAttributeChangeEvent(EventLoggingManager.EVENT_NAME_CHANGE_ATTRIBUTE_ADD, newAttribute.typeId, newAttribute.objectId, tracker.objectId)
 
-            attributeListAdapter.notifyItemInserted(tracker.attributes.size - 1)
+            //attributeListAdapter.notifyItemInserted(tracker.attributes.size - 1)
             scrollToBottomReserved = true
         }
     }
