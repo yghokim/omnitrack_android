7396215b4bf3495fdc47856e8636bc92f8e43bac
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-05-06 18:38:38 +0900

    Reboot receiver fires after reboot.

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/receivers/RebootReceiver.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/receivers/RebootReceiver.kt
@@ -3,6 +3,7 @@ package kr.ac.snu.hcil.omnitrack.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.PowerManager
 import kr.ac.snu.hcil.omnitrack.OTApplication
 import kr.ac.snu.hcil.omnitrack.core.system.OTShortcutPanelManager
 import kr.ac.snu.hcil.omnitrack.services.OTVersionCheckService
@@ -12,18 +13,30 @@ import rx.schedulers.Schedulers
  * Created by Young-Ho on 9/4/2016.
  */
 class RebootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        println("OMNITRACK: reboot receiver called")
-        when (intent.action) {
-            Intent.ACTION_BOOT_COMPLETED -> {
-                println("OMNITRACK: Android system rebooted")
-                OTApplication.app.currentUserObservable.observeOn(Schedulers.immediate()).subscribe({
-                    user ->
-                    OTShortcutPanelManager.refreshNotificationShortcutViews(user, context)
-                },{})
-
-                OTVersionCheckService.setupServiceAlarm(context)
-            }
-        }
+    companion object {
+        const val TAG = "RebootReceiver"
     }
+    override fun onReceive(context: Context, intent: Intent) {
+        println("OMNITRACK: reboot receiver called - ${intent.action}")
+
+        val result = goAsync()
+        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
+        wl.acquire()
+
+
+        OTVersionCheckService.setupServiceAlarm(context)
+
+        OTApplication.app.currentUserObservable.observeOn(Schedulers.newThread()).subscribe({
+            user ->
+            OTShortcutPanelManager.refreshNotificationShortcutViews(user, context)
+
+        }, {}, {
+            result.finish()
+            wl.release()
+            println("OMNITRACK: reboot receiver finished.")
+        })
+
+    }
+
 }
