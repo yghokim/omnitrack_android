df049cc2ff53c0d1e7863522e2ce0534aa3145cb
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-06-01 14:42:23 +0900

    replaced Interval Time Trigger calculation with new calculator

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/time/TimeScheduleCalculator.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/utils/time/TimeScheduleCalculator.kt
@@ -1,14 +1,34 @@
 package kr.ac.snu.hcil.omnitrack.utils.time
 
+import kr.ac.snu.hcil.omnitrack.utils.BitwiseOperationHelper
+
 /**
  * Created by Young-Ho on 5/31/2017.
  */
-abstract class TimeScheduleCalculator {
+abstract class TimeScheduleCalculator<T> where T : TimeScheduleCalculator<T> {
 
-    var availableDaysOfWeek = BooleanArray(7, { i -> true })
+    private var availableDaysOfWeek = BooleanArray(7, { i -> true })
     var endAt: Long = Long.MAX_VALUE
 
-    fun calculateNext(last: Long, now: Long): Long? {
+    protected fun isAvailableDayOfWeek(dayOfWeek: Int): Boolean {
+        return availableDaysOfWeek[dayOfWeek - 1]
+    }
+
+    fun setEndAt(endAt: Long): T {
+        this.endAt = endAt
+        return this as T
+    }
+
+    fun setAvailableDaysOfWeekFlag(flags: Int = 0b1111111): T {
+        for (i in (0..6)) {
+            availableDaysOfWeek[i] = BitwiseOperationHelper.getBooleanAt(flags, 6 - i)
+
+        }
+
+        return this as T
+    }
+
+    fun calculateNext(last: Long?, now: Long): Long? {
         if (now > endAt) {
             return null
         } else {
