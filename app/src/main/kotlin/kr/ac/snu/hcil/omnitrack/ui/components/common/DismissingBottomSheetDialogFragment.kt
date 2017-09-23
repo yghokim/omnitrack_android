525556197c50f0da0f9f9f2ae1557909d4528ddf
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-09-24 00:24:22 +0900

    preparing bottomsheetfragment

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/components/common/DismissingBottomSheetDialogFragment.kt
@@ -0,0 +1,7 @@
+package kr.ac.snu.hcil.omnitrack.ui.components.common
+
+/**
+ * Created by younghokim on 2017. 9. 23..
+ */
+class DismissableBottomSheetFragment {
+}
---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/items/RecentItemValuePickerBottomSheetFragment.kt
@@ -0,0 +1,7 @@
+package kr.ac.snu.hcil.omnitrack.ui.pages.items
+
+/**
+ * Created by younghokim on 2017. 9. 21..
+ */
+class RecentItemValuePickerBottomSheetFragment {
+}
