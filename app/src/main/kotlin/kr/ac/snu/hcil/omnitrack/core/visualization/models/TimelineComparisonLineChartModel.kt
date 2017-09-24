c17a85b1ba2f2ac8ceff5e0965ddb7e753d26dba
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-09-24 22:33:08 +0900

    extracted IDatabaseManager interface

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/visualization/models/TimelineComparisonLineChartModel.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/visualization/models/TimelineComparisonLineChartModel.kt
@@ -7,6 +7,7 @@ import kr.ac.snu.hcil.omnitrack.core.OTTracker
 import kr.ac.snu.hcil.omnitrack.core.attributes.OTAttribute
 import kr.ac.snu.hcil.omnitrack.core.attributes.OTNumberAttribute
 import kr.ac.snu.hcil.omnitrack.core.database.DatabaseManager
+import kr.ac.snu.hcil.omnitrack.core.database.IDatabaseManager
 import kr.ac.snu.hcil.omnitrack.core.visualization.CompoundAttributeChartModel
 import kr.ac.snu.hcil.omnitrack.core.visualization.interfaces.ILineChartOnTime
 import kr.ac.snu.hcil.omnitrack.ui.components.visualization.AChartDrawer
@@ -32,7 +33,7 @@ class TimelineComparisonLineChartModel(override val attributes: List<OTNumberAtt
         xScale.setDomain(getTimeScope().from, getTimeScope().to)
         xScale.quantize(currentGranularity)
 
-        return DatabaseManager.loadItems(parent, getTimeScope(), DatabaseManager.Order.ASC).map {
+        return DatabaseManager.loadItems(parent, getTimeScope(), IDatabaseManager.Order.ASC).map {
             items ->
 
             var currentItemPointer = 0
