dfc2e15467d3f58473b3a6665cdb649368259d59
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-09-27 12:57:29 +0900

    preparing Synchronization abstraction

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/database/abstraction/ASynchronizationService.kt
@@ -0,0 +1,8 @@
+package kr.ac.snu.hcil.omnitrack.core.database.abstraction
+
+import android.app.Service
+
+/**
+ * Created by younghokim on 2017. 9. 26..
+ */
+abstract class ASynchronizationService : Service()
---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/database/synchronization/ESyncDataType.kt
@@ -0,0 +1,8 @@
+package kr.ac.snu.hcil.omnitrack.core.database.synchronization
+
+/**
+ * Created by younghokim on 2017. 9. 27..
+ */
+enum class ESyncDataType {
+    USER, TRACKER, TRIGGER, ITEM
+}
