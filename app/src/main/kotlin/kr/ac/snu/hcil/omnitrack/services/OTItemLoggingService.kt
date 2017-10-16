a23424b449ddbdd6a5ee22896f73465d64128afb
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-10-17 02:34:34 +0900

    required field information check at first.

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/services/OTItemLoggingService.kt
@@ -0,0 +1,16 @@
+package kr.ac.snu.hcil.omnitrack.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+ * Created by Young-Ho on 10/17/2017.
+ */
+class OTItemLoggingService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+
+}
