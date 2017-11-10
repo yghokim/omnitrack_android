bad80b512662e4ba0511faafdf4d3be9b4a9a7c1
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-11-10 18:10:43 +0900

    refactored some LINT errors.

--- app/src/main/kotlin/com/zhaoxiaodan/miband/listeners/RealtimeStepsNotifyListener.kt
+++
@@ -1,5 +0,0 @@
-package com.zhaoxiaodan.miband.listeners
-
-interface RealtimeStepsNotifyListener {
-    fun onNotify(steps: Int)
-}
--- app/src/main/kotlin/com/zhaoxiaodan/miband/model/VibrationMode.kt
+++
@@ -1,8 +0,0 @@
-/**
-
- */
-package com.zhaoxiaodan.miband.model
-
-enum class VibrationMode {
-    VIBRATION_WITH_LED, VIBRATION_10_TIMES_WITH_LED, VIBRATION_WITHOUT_LED
-}
