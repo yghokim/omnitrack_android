20854adb058ed209c9d62747f039e69a1230ac7e
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-11-15 00:21:22 +0900

    shows script indicator on trigger list remove realm if migration needed only in Debug mode.

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/di/BackendDatabaseModule.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/di/BackendDatabaseModule.kt
@@ -2,9 +2,9 @@ package kr.ac.snu.hcil.omnitrack.core.di
 
 import dagger.Module
 import dagger.Provides
+import io.realm.BuildConfig
 import io.realm.Realm
 import io.realm.RealmConfiguration
-import kr.ac.snu.hcil.omnitrack.core.database.local.RealmDatabaseManager
 import javax.inject.Singleton
 
 /**
@@ -15,12 +15,19 @@ class BackendDatabaseModule {
 
     @Provides
     @Singleton
-    fun backendDatabaseConfiguration(): RealmDatabaseManager.Configuration {
-        return RealmDatabaseManager.Configuration()
+    fun backendDatabaseConfiguration(): RealmConfiguration {
+        return RealmConfiguration.Builder()
+                .name("backend.db")
+                .run {
+                    if (BuildConfig.DEBUG) {
+                        this.deleteRealmIfMigrationNeeded()
+                    } else this
+                }
+                .build()
     }
 
     @Provides
-    fun makeBackendDbRealm(configuration: RealmDatabaseManager.Configuration): Realm {
-        return Realm.getInstance(RealmConfiguration.Builder().name(configuration.fileName).build())
+    fun makeBackendDbRealm(configuration: RealmConfiguration): Realm {
+        return Realm.getInstance(configuration)
     }
 }
