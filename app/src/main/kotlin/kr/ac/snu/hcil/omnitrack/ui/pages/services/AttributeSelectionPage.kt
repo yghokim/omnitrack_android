0c26ad913ea0260bd8c1c40dea85eefe8df7ff5c
JunhoeKim <joon.hoe94@gmail.com>
2017-12-27 01:18:39 +0900

    remove debugging log

--- app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/services/AttributeSelectionPage.kt
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/ui/pages/services/AttributeSelectionPage.kt
@@ -6,7 +6,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.text.InputType
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -79,10 +78,6 @@ class AttributeSelectionPage(override val parent : ServiceWizardView) : AWizardP
             attributes.addAll(validAttributes)
             attributes.removeAll { !currentMeasureFactory.isAttachableTo(it) }
             attributeListView?.adapter?.notifyDataSetChanged()
-            Log.i("TAG", "${attributes.size}")
-            attributes.forEach {
-                attribute -> Log.i("TAG", attribute.name)
-            }
         }
     }
 
