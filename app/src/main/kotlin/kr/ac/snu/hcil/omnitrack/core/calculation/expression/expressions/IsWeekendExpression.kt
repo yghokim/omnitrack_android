af8123e61c358a6cd57334b830ebd883e052fcba
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2018-01-23 21:20:55 +0900

    added isWeekend script command.

---
+++ app/src/main/kotlin/kr/ac/snu/hcil/omnitrack/core/calculation/expression/expressions/IsWeekendExpression.kt
@@ -0,0 +1,22 @@
+package kr.ac.snu.hcil.omnitrack.core.calculation.expression.expressions
+
+import com.udojava.evalex.Expression
+import kr.ac.snu.hcil.omnitrack.core.calculation.expression.ExpressionConstants
+import kr.ac.snu.hcil.omnitrack.core.calculation.expression.LongLazyNumber
+import kr.ac.snu.hcil.omnitrack.utils.getDayOfWeek
+import java.util.*
+
+/**
+ * Created by younghokim on 2017. 11. 14..
+ */
+class IsWeekendExpression : Expression.LazyFunction(ExpressionConstants.COMMAND_IS_WEEKEND, 0, true) {
+
+    override fun lazyEval(lazyParams: MutableList<Expression.LazyNumber>?): Expression.LazyNumber {
+        val cal = Calendar.getInstance()
+        val dow = cal.getDayOfWeek()
+        return LongLazyNumber(if (dow == 1 || dow == 7) {
+            1
+        } else 0)
+    }
+
+}
