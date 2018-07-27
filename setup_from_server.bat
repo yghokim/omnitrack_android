771f3dd79197ae5a75543bf336b7a07628af73fe
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2018-07-27 23:38:13 +0900

    added setup files for server build.

---
+++ setup_from_server.bat
@@ -0,0 +1,15 @@
+@echo update git submodules.
+git submodule update --init --recursive
+git submodule foreach "(git checkout master; git pull)&"
+
+@echo setup visualization javascripts.
+cd omnitrack_visualization_core
+call npm install
+
+@echo pack the javascript files. This process may take about a minute...
+
+call node_modules\.bin\webpack.cmd
+cd ..
+xcopy /E "omnitrack_visualization_core/built" "app/src/main/assets/web/visualization"
+
+@echo Now you are ready to build the OmniTrack application. Enjoy!
---
+++ setup_from_server.sh
@@ -0,0 +1,6 @@
+git submodule update --init --recursive
+git submodule foreach "(git checkout master; git pull)&"
+cd omnitrack_visualization_core && npm install
+node_modules/.bin/webpack
+cd ..
+cp -a omnitrack_visualization_core/built/. ./app/src/main/assets/web/visualization/
